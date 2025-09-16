c43a06eff2a0ee11bc8b5c1924eaf842083f811a
Jozefoslawnaswoim <jozefoslawnaswoim@outlook.com>
2025-09-16 08:29:06 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ export default function App() {
       <Header />
       <main>
         <Hero />
+        <QuickStats />       
         <WhyNow />
         <Roadmap /><HowToHelp />
         <FAQ />
